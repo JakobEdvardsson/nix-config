853ce1a8bf520f8bfcedcfc221fa5b6ebc4933ee
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-01-10 17:30:23 +0100

    Initial setup for legion

---
+++ hosts/legion/default-configuration.nix
@@ -0,0 +1,59 @@
+
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jakobe = {
+    isNormalUser = true;
+    description = "Jakob Edvardsson";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+      kate
+    #  thunderbird
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # Development stuff
+    vim
+    vscode
+    wget
+    git
+    github-desktop
+  ];
+
+
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
