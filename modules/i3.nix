4580db262a900e01de54c8277852d196c407d492
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-01-13 17:03:48 +0100

    Small changes

--- modules/i3.nix
+++ modules/i3.nix
@@ -1,10 +1,6 @@
-{pkgs, ...}:
-
-
-{
-
+{pkgs, ...}: {
   # i3 related options
-  environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw
+  environment.pathsToLink = ["/libexec"]; # links /libexec from derivations to /run/current-system/sw
   services.xserver = {
     enable = true;
 
@@ -13,34 +9,34 @@
     };
 
     displayManager = {
-        defaultSession = "none+i3";
-        lightdm.enable = false;
-        gdm.enable = true;
+      defaultSession = "none+i3";
+      lightdm.enable = false;
+      gdm.enable = true;
     };
 
     windowManager.i3 = {
       enable = true;
       extraPackages = with pkgs; [
-        rofi          # application launcher, the same as dmenu
-        dunst         # notification daemon
-        i3blocks      # status bar
-        i3lock        # default i3 screen locker
-        xautolock     # lock screen after some time
-        i3status      # provide information to i3bar
-        i3-gaps       # i3 with gaps
-        picom         # transparency and shadows
-        feh           # set wallpaper
-        acpi          # battery information
-        arandr        # screen layout manager
-        dex           # autostart applications
-        xbindkeys     # bind keys to commands
-        xorg.xbacklight  # control screen brightness
-        xorg.xdpyinfo      # get screen information
-        sysstat       # get system information
-        autotiling    # auto tilingcd
-        pywal         # generate colorprofile
+        rofi # application launcher, the same as dmenu
+        dunst # notification daemon
+        i3blocks # status bar
+        i3lock # default i3 screen locker
+        xautolock # lock screen after some time
+        i3status # provide information to i3bar
+        i3-gaps # i3 with gaps
+        picom # transparency and shadows
+        feh # set wallpaper
+        acpi # battery information
+        arandr # screen layout manager
+        dex # autostart applications
+        xbindkeys # bind keys to commands
+        brightnessctl # control screen brightness
+        xorg.xdpyinfo # get screen information
+        sysstat # get system information
+        autotiling # auto tilingcd
+        pywal # generate colorprofile
         networkmanagerapplet
-     ];
+      ];
     };
   };
 
