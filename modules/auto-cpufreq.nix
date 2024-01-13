de6bbf8e351b7a16acbc85deb2609ffe70f25ee1
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-01-13 17:05:04 +0100

    Create auto-cpufreq.nix

---
+++ modules/auto-cpufreq.nix
@@ -0,0 +1,14 @@
+{...}: {
+  services.tlp.enable = false;
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+      governor = "powersave";
+      turbo = "never";
+    };
+    charger = {
+      governor = "performance";
+      turbo = "auto";
+    };
+  };
+}
