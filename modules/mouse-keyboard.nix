13810fbfe528af143f90e25976c23bf6b936c18e
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-01-14 19:57:30 +0100

    touchpad speed

--- modules/mouse-keyboard.nix
+++ modules/mouse-keyboard.nix
@@ -1,11 +1,11 @@
 {...}: {
   services.xserver = {
     libinput.touchpad.naturalScrolling = true;
-    #synaptics.accelFactor = "1.0";
+    # synaptics.accelFactor = "1.0";
     libinput = {
       enable = true;
       mouse.accelProfile = "flat";
-      #mouse.accelSpeed = "1.0";
+      mouse.accelSpeed = "1.0";
     };
   };
 }
