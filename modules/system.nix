f1aecf4c45f86a6a5ca439560a2069d7c0c769af
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-01-14 14:50:06 +0100

    Restructure and update to use latest kernel

--- modules/system.nix
+++ modules/system.nix
@@ -5,6 +5,13 @@
   pkgs,
   ...
 }: {
+  imports = [
+    ./mouse-keyboard.nix
+    ./locale.nix
+    ./fonts.nix
+    ./polkit-keyring.nix
+  ];
+
   # This will add each flake input as a registry
   # To make nix3 commands consistent with your flake
   nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
@@ -27,6 +34,14 @@
     auto-optimise-store = true;
   };
 
+  # Packages that are always installed
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    curl
+    git
+  ];
+
   # Get battery info
   services.upower.enable = true;
 
@@ -37,52 +52,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Stockholm";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "sv_SE.UTF-8";
-    LC_IDENTIFICATION = "sv_SE.UTF-8";
-    LC_MEASUREMENT = "sv_SE.UTF-8";
-    LC_MONETARY = "sv_SE.UTF-8";
-    LC_NAME = "sv_SE.UTF-8";
-    LC_NUMERIC = "sv_SE.UTF-8";
-    LC_PAPER = "sv_SE.UTF-8";
-    LC_TELEPHONE = "sv_SE.UTF-8";
-    LC_TIME = "sv_SE.UTF-8";
-  };
-
-  services.xserver = {
-    libinput.touchpad.naturalScrolling = true;
-    layout = "se";
-    xkbVariant = "";
-  };
-  # Configure console keymap
-  console.keyMap = "sv-latin1";
-
   # do garbage collection weekly to keep disk usage low
   nix.gc = {
     automatic = lib.mkDefault true;
     dates = lib.mkDefault "weekly";
     options = lib.mkDefault "--delete-older-than 10d";
-  };
-
-  systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = ["graphical-session.target"];
-      wants = ["graphical-session.target"];
-      after = ["graphical-session.target"];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
   };
 }
