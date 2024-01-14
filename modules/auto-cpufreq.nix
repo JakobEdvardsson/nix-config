62740e386d09bfd84d69f145b9919a6493c7c134
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-01-14 18:04:35 +0100

    Update auto-cpufreq.nix

--- modules/auto-cpufreq.nix
+++ modules/auto-cpufreq.nix
@@ -1,10 +1,14 @@
 {...}: {
   services.tlp.enable = false;
+  services.power-profiles-daemon.enable = false;
+  powerManagement.powertop.enable = false;
+
   services.auto-cpufreq.enable = true;
   services.auto-cpufreq.settings = {
     battery = {
       governor = "powersave";
       turbo = "never";
+      scaling_max_freq = 1500000;
     };
     charger = {
       governor = "performance";
