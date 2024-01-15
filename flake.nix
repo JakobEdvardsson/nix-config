cf41b7d0c6c8ba9f40a332a5d3d8961010040613
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-01-15 13:54:01 +0100

    Added NUR

--- flake.nix
+++ flake.nix
@@ -9,12 +9,13 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # TODO: Add any other flake you might need
     nixos-hardware.url = "github:JakobEdvardsson/nixos-hardware/master";
+
+    nur.url = "github:nix-community/NUR";
+
     # Shameless plug: looking for a way to nixify your themes and make
     # everything match nicely? Try nix-colors!
     # nix-colors.url = "github:misterio77/nix-colors";
-
   };
 
   outputs = {
@@ -22,6 +23,7 @@
     nixpkgs,
     home-manager,
     nixos-hardware,
+    nur,
     ...
   } @ inputs: let
     inherit (self) outputs;
@@ -34,20 +36,24 @@
         # > Our main nixos configuration file <
         modules = [
           ./hosts/legion
-          home-manager.nixosModules.home-manager
           nixos-hardware.nixosModules.lenovo-legion-16achg6-hybrid
+
+          home-manager.nixosModules.home-manager
           {
             #home-manager.useGlobalPkgs = true;
             #home-manager.useUserPackages = true;
-            home-manager.users.jakobe = import ./hosts/legion/home.nix;
-
+            home-manager.users.jakobe = {
+              imports = [
+                nur.nixosModules.nur
+                ./hosts/legion/home.nix
+              ];
+            };
 
             home-manager.extraSpecialArgs = {inherit inputs outputs;};
           }
         ];
       };
     };
-
 
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
