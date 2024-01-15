880c6f66639d297ddb0b39b57619f8d37eb8615a
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-01-15 16:33:38 +0100

    Firefox setup not complete

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -4,11 +4,25 @@
   ...
 }: {
   programs = {
-    
-
     firefox = {
       enable = true;
-      #profiles.ryan = {};
+      profiles = {
+        jakob = {
+          isDefault = true;
+          settings = {
+            "browser.startup.homepage" = "about:home";
+            "browser.aboutConfig.showWarning" = false;
+          };
+          search.default = "DuckDuckGo";
+          extensions = with config.nur.repos.rycee.firefox-addons; [
+            ublock-origin
+            bitwarden
+          ];
+          # search = {
+          #   default = "DuckDuckGo";
+          # };
+        };
+      };
     };
   };
 }
--- modules/power/tlp.nix
+++ modules/power/tlp.nix
@@ -20,8 +20,8 @@
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       PLATFORM_PROFILE_ON_BAT = "low-power";
       CPU_SCALING_MAX_FREQ_ON_BAT = "1500000";
-      CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 20;
+      #CPU_MIN_PERF_ON_BAT = 0;
+      #CPU_MAX_PERF_ON_BAT = 20;
       CPU_BOOST_ON_BAT = 0;
       CPU_HWP_DYN_BOOST_ON_BAT = 0;
     };
