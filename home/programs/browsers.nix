8992fa1000a99786d235f3f82364334f0f5148fc
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-01-15 21:27:59 +0100

    Disabled Firefox configs

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -7,21 +7,21 @@
     firefox = {
       enable = true;
       profiles = {
-        jakob = {
-          isDefault = true;
-          settings = {
-            "browser.startup.homepage" = "about:home";
-            "browser.aboutConfig.showWarning" = false;
-          };
-          search.default = "DuckDuckGo";
-          extensions = with config.nur.repos.rycee.firefox-addons; [
-            ublock-origin
-            bitwarden
-          ];
-          # search = {
-          #   default = "DuckDuckGo";
-          # };
-        };
+        #jakob = {
+        #  isDefault = true;
+        # settings = {
+        #  "browser.startup.homepage" = "about:home";
+        # "browser.aboutConfig.showWarning" = false;
+        #};
+        #search.default = "DuckDuckGo";
+        #extensions = with config.nur.repos.rycee.firefox-addons; [
+        #  ublock-origin
+        #  bitwarden
+        #];
+        # search = {
+        #   default = "DuckDuckGo";
+        # };
+        #};
       };
     };
   };
