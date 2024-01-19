fb4b6cec515186b8eb5b29bde9e9bba64a24e43d
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-01-19 15:23:24 +0100

    Create virtualbox.nix

---
+++ common/programs/virtualbox.nix
@@ -0,0 +1,9 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  users.extraGroups.vboxusers.members = ["jakobe"];
+}
