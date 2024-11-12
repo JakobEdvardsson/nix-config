398efe247c372843f6a8656fa87671a66f8edfe2
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-11-12 10:39:13 +0100

    remove ollama temporary

--- hosts/servox/config.nix
+++ hosts/servox/config.nix
@@ -422,18 +422,20 @@
   };
 
   # Ollama
-  services.ollama = {
-    #package = pkgs.unstable.ollama; # Uncomment if you want to use the unstable channel, see https://fictionbecomesfact.com/nixos-unstable-channel
-    enable = true;
-    acceleration = "rocm"; # Or "rocm"
-    rocmOverrideGfx = "10.3.1";
-    #environmentVariables = { # I haven't been able to get this to work myself yet, but I'm sharing it for the sake of completeness
-    # HOME = "/home/ollama";
-    # OLLAMA_MODELS = "/home/ollama/models";
-    # OLLAMA_HOST = "0.0.0.0:11434"; # Make Ollama accesible outside of localhost
-    # OLLAMA_ORIGINS = "http://localhost:8080,http://192.168.0.10:*"; # Allow access, otherwise Ollama returns 403 forbidden due to CORS
-    #};
-  };
+  /*
+    services.ollama = {
+      #package = pkgs.unstable.ollama; # Uncomment if you want to use the unstable channel, see https://fictionbecomesfact.com/nixos-unstable-channel
+      enable = true;
+      acceleration = "rocm"; # Or "rocm"
+      rocmOverrideGfx = "10.3.1";
+      #environmentVariables = { # I haven't been able to get this to work myself yet, but I'm sharing it for the sake of completeness
+      # HOME = "/home/ollama";
+      # OLLAMA_MODELS = "/home/ollama/models";
+      # OLLAMA_HOST = "0.0.0.0:11434"; # Make Ollama accesible outside of localhost
+      # OLLAMA_ORIGINS = "http://localhost:8080,http://192.168.0.10:*"; # Allow access, otherwise Ollama returns 403 forbidden due to CORS
+      #};
+    };
+  */
 
   # OpenGL
   hardware.graphics = {
