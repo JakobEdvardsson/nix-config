e2ccf38fd43367d54516a87573975974b7b376ce
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-11-20 11:44:09 +0100

    Linger and open ports

--- hosts/servox/config.nix
+++ hosts/servox/config.nix
@@ -73,6 +73,15 @@
 
   };
 
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [
+    8080
+    11434
+  ];
+  networking.firewall.allowedUDPPorts = [ ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = true;
+
   #services.power-profiles-daemon.enable = true;
   hardware.cpu.amd.updateMicrocode = true;
   hardware.enableRedistributableFirmware = lib.mkDefault true;
@@ -169,12 +178,6 @@
   };
 
   documentation.man.generateCaches = false;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
