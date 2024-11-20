7bf89c42c243c53e10fc53c2d4fb2e5007780baf
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-11-20 11:58:13 +0100

    Update nvim config

--- home-manager/nvim/plugins/treesitter.lua
+++ home-manager/nvim/plugins/treesitter.lua
@@ -1,28 +1,28 @@
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
+-- import nvim-treesitter plugin
+local treesitter = require("nvim-treesitter.configs")
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
+-- configure treesitter
+treesitter.setup({ -- enable syntax highlighting
+	highlight = {
+		enable = true,
+	},
 
-      auto_install = false,
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = {},
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
+	auto_install = false,
+	-- enable indentation
+	indent = { enable = true },
+	-- enable autotagging (w/ nvim-ts-autotag plugin)
+	autotag = {
+		enable = true,
+	},
+	-- ensure these language parsers are installed
+	ensure_installed = {},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<C-space>",
+			node_incremental = "<C-space>",
+			scope_incremental = false,
+			node_decremental = "<bs>",
+		},
+	},
+})
