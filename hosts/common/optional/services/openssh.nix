d6372a25f59775f6dfc4a084abcff18bb749eea8
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2025-01-16 19:45:10 +0100

    Add openssh

--- hosts/common/optional/services/openssh.nix
+++ hosts/common/optional/services/openssh.nix
@@ -5,12 +5,8 @@
   ...
 }:
 let
-  sshPort = config.hostSpec.networking.ports.tcp.ssh;
-
-  # Sops needs access to the keys before the persist dirs are even mounted; so
-  # just persisting the keys won't work, we must point at /persist
-  #FIXME:(impermanence) refactor this to how fb did it
-  hasOptinPersistence = false;
+  # sshPort = config.hostSpec.networking.ports.tcp.ssh;
+  sshPort = 22;
 in
 
 {
@@ -23,35 +19,18 @@ in
       PasswordAuthentication = false;
       PermitRootLogin = "no";
       # Automatically remove stale sockets
-      StreamLocalBindUnlink = "yes";
+      # StreamLocalBindUnlink = "yes";
       # Allow forwarding ports to everywhere
-      GatewayPorts = "clientspecified";
+      # GatewayPorts = "clientspecified";
     };
 
     hostKeys = [
       {
-        path = "${lib.optionalString hasOptinPersistence "/persist"}/etc/ssh/ssh_host_ed25519_key";
+        path = "/etc/ssh/ssh_host_ed25519_key";
         type = "ed25519";
       }
     ];
+
+    openFirewall = true;
   };
-
-  # yubikey login / sudo
-  # NOTE: We use rssh because sshAgentAuth is old and doesn't support yubikey:
-  # https://github.com/jbeverly/pam_ssh_agent_auth/issues/23
-  # https://github.com/z4yx/pam_rssh
-  security.pam.services.sudo =
-    { config, ... }:
-    {
-      rules.auth.rssh = {
-        order = config.rules.auth.ssh_agent_auth.order - 1;
-        control = "sufficient";
-        modulePath = "${pkgs.pam_rssh}/lib/libpam_rssh.so";
-        settings.authorized_keys_command = pkgs.writeShellScript "get-authorized-keys" ''
-          cat "/etc/ssh/authorized_keys.d/$1"
-        '';
-      };
-    };
-
-  networking.firewall.allowedTCPPorts = [ sshPort ];
 }
--- hosts/nixos/servox/default.nix
+++ hosts/nixos/servox/default.nix
@@ -57,7 +57,7 @@
 
       "hosts/common/optional/hyprland.nix" # window manager
       # "hosts/common/optional/services/greetd.nix" # display manager
-      # "hosts/common/optional/services/openssh.nix" # allow remote SSH access
+      "hosts/common/optional/services/openssh.nix" # allow remote SSH access
       # "hosts/common/optional/services/printing.nix" # CUPS
       "hosts/common/optional/audio.nix" # pipewire and cli controls
       "hosts/common/optional/bluetooth.nix" # bluetooth and blueman
