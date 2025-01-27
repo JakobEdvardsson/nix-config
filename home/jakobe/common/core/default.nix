9312d06f75735772030dbecde304a596dd2adc7b
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2025-01-27 20:18:34 +0100

    remove xdg override

--- home/jakobe/common/core/default.nix
+++ home/jakobe/common/core/default.nix
@@ -50,20 +50,24 @@
     userDirs = {
       enable = true;
       createDirectories = true;
-      desktop = "${config.home.homeDirectory}/.desktop";
-      documents = "${config.home.homeDirectory}/doc";
-      download = "${config.home.homeDirectory}/downloads";
-      music = "${config.home.homeDirectory}/media/audio";
-      pictures = "${config.home.homeDirectory}/media/images";
-      videos = "${config.home.homeDirectory}/media/video";
+      /*
+        desktop = "${config.home.homeDirectory}/.desktop";
+        documents = "${config.home.homeDirectory}/doc";
+        download = "${config.home.homeDirectory}/downloads";
+        music = "${config.home.homeDirectory}/media/audio";
+        pictures = "${config.home.homeDirectory}/media/images";
+        videos = "${config.home.homeDirectory}/media/video";
+      */
       # publicshare = "/var/empty"; #using this option with null or "/var/empty" barfs so it is set properly in extraConfig below
       # templates = "/var/empty"; #using this option with null or "/var/empty" barfs so it is set properly in extraConfig below
 
-      extraConfig = {
-        # publicshare and templates defined as null here instead of as options because
-        XDG_PUBLICSHARE_DIR = "/var/empty";
-        XDG_TEMPLATES_DIR = "/var/empty";
-      };
+      /*
+        extraConfig = {
+          # publicshare and templates defined as null here instead of as options because
+          XDG_PUBLICSHARE_DIR = "/var/empty";
+          XDG_TEMPLATES_DIR = "/var/empty";
+        };
+      */
     };
   };
 
