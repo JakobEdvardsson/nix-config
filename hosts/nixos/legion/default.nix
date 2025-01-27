db581f104436ea645d4e206beb669cb17776dd95
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2025-01-27 20:10:09 +0100

    Add specialisation for legion

--- hosts/nixos/legion/default.nix
+++ hosts/nixos/legion/default.nix
@@ -75,9 +75,20 @@
 
   ];
 
-  #
-  # ========== Host Specification ==========
-  #
+  specialisation = {
+    on-the-go.configuration = {
+      system.nixos.tags = [ "on-the-go" ];
+      hardware.nvidia = {
+        prime.offload.enable = lib.mkForce true;
+        prime.offload.enableOffloadCmd = lib.mkForce true;
+        prime.sync.enable = lib.mkForce false;
+      };
+
+      imports = [
+        inputs.hardware.nixosModules.common-gpu-nvidia-disable
+      ];
+    };
+  };
 
   hostSpec = {
     hostName = "legion";
