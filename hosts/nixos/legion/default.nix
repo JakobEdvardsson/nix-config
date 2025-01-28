fee78ca6d8bde1941ca27acf01fd24eae37b81d1
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2025-01-28 09:13:46 +0100

    stable kernel & nvidia drivers

--- hosts/nixos/legion/default.nix
+++ hosts/nixos/legion/default.nix
@@ -113,7 +113,7 @@
     systemd.enable = true;
   };
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages;
 
   # https://wiki.nixos.org/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.05";
