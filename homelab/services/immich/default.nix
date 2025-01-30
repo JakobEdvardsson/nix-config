9514522f88e3b5f766008f9906e500787959c1dc
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2025-01-30 19:13:24 +0100

    Add notthebee homelab setup

---
+++ homelab/services/immich/default.nix
@@ -0,0 +1,52 @@
+{ config, lib, ... }:
+let
+  cfg = config.homelab.services.immich;
+  homelab = config.homelab;
+in
+{
+  options.homelab.services.immich = {
+    enable = lib.mkEnableOption "Self-hosted photo and video management solution";
+    user = lib.mkOption {
+      default = config.homelab.user;
+      type = lib.types.str;
+      description = ''
+        User to run the Immich container as
+      '';
+    };
+    group = lib.mkOption {
+      default = config.homelab.group;
+      type = lib.types.str;
+      description = ''
+        Group to run the Immich container as
+      '';
+    };
+    mediaDir = lib.mkOption {
+      type = lib.types.path;
+      default = "${config.homelab.mounts.fast}/Photos/Immich";
+    };
+    url = lib.mkOption {
+      type = lib.types.str;
+      default = "photos.${homelab.baseDomain}";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    systemd.tmpfiles.rules = [ "d ${cfg.mediaDir} 0775 immich ${homelab.group} - -" ];
+    users.users.immich.extraGroups = [
+      "video"
+      "render"
+    ];
+    services.immich = {
+      group = homelab.group;
+      enable = true;
+      port = 2283;
+      mediaLocation = "${cfg.mediaDir}";
+    };
+    services.caddy.virtualHosts."${cfg.url}" = {
+      useACMEHost = homelab.baseDomain;
+      extraConfig = ''
+        reverse_proxy http://${config.services.immich.host}:${toString config.services.immich.port}
+      '';
+    };
+  };
+
+}
