7b5f10ff192b1c16e91fddf821fa3110c7be640c
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2025-02-08 21:46:16 +0100

    add usb mount

--- hosts/nixos/think/default.nix
+++ hosts/nixos/think/default.nix
@@ -45,6 +45,20 @@
   #
   # ========== Host Specification ==========
   #
+  fileSystems.${config.homelab.mounts.slow} = {
+    device = "/dev/disk/by-uuid/1e3795b5-c1e2-4c6d-9663-1600b4efa33e";
+    options = [
+      "defaults"
+      "noatime" # Avoids writing access times, improves performance
+      "errors=remount-ro" # Remount as read-only on failure
+      "x-mount.mkdir" # Creates the mount directory if missing
+      "uid=994" # Set owner to UID 994
+      "gid=993" # Set group to GID 993
+      "umask=002" # Ensure group has write permission
+      "nofail" # Prevent system from failing if this drive doesn't mount
+    ];
+    fsType = "ext4";
+  };
 
   hostSpec = {
     hostName = "think";
