b9baf9c9181a3b1a02f115035b7ad27f64355401
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2025-02-09 08:36:52 +0100

    add mount for ssd

--- hosts/nixos/think/default.nix
+++ hosts/nixos/think/default.nix
@@ -57,6 +57,18 @@
     fsType = "ext4";
   };
 
+  fileSystems.${config.homelab.mounts.fast} = {
+    device = "/dev/disk/by-uuid/e19d961a-c436-452d-8735-2be06a7e296d";
+    options = [
+      "defaults"
+      "noatime" # Avoids writing access times, improves performance
+      "errors=remount-ro" # Remount as read-only on failure
+      "x-mount.mkdir" # Creates the mount directory if missing
+      "nofail" # Prevent system from failing if this drive doesn't mount
+    ];
+    fsType = "ext4";
+  };
+
   hostSpec = {
     hostName = "think";
   };
