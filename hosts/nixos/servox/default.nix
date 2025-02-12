030db3e062c22c871e35366d016cda8f5c2a79d2
Jakob Edvardsson <jakob@edvardsson.tech>
2025-02-12 18:13:21 +0100

    feat: add swapfile and ollama

--- hosts/nixos/servox/default.nix
+++ hosts/nixos/servox/default.nix
@@ -78,6 +78,21 @@
   #
   # ========== Host Specification ==========
   #
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 16 * 1024;
+    }
+  ];
+
+  services.ollama = {
+    enable = true;
+    acceleration = "rocm";
+    environmentVariables = {
+      HCC_AMDGPU_TARGET = "gfx1031"; # used to be necessary, but doesn't seem to anymore
+    };
+    rocmOverrideGfx = "10.3.1";
+  };
 
   hostSpec = {
     hostName = "servox";
