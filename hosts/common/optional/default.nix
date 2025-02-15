23bdb87a339289137a4cc2a95cf103c31c9255ba
Jakob Edvardsson <jakob@edvardsson.tech>
2025-02-15 18:57:29 +0100

    feat: add quicksync module

---
+++ hosts/common/optional/default.nix
@@ -0,0 +1,11 @@
+# TODO make it so that all files in optional are options
+{
+  lib,
+  ...
+}:
+{
+  #imports = (lib.custom.scanPaths ./.);
+  imports = [
+    ./intel-quicksync.nix
+  ];
+}
---
+++ hosts/common/optional/intel-quicksync.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+let
+  cfg = config.customOption.intel-quicksync;
+in
+{
+
+  options.customOption.intel-quicksync = {
+    enable = lib.mkEnableOption "Enable intel-quicksync";
+  };
+
+  config = lib.mkIf cfg.enable {
+    # intel hardware acceleration
+    nixpkgs.config.packageOverrides = pkgs: {
+      vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+    };
+    hardware = {
+      enableRedistributableFirmware = true;
+      cpu.intel.updateMicrocode = true;
+      graphics = {
+        enable = true;
+        extraPackages = with pkgs; [
+          intel-media-driver
+          intel-vaapi-driver
+          vaapiVdpau
+          intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+          vpl-gpu-rt # QSV on 11th gen or newer
+        ];
+      };
+    };
+  };
+}
