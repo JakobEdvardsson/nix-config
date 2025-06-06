8c8dddc37b7a5a428b0bb6b2f7260f5b5361e65f
Jakob Edvardsson <jakob@edvardsson.tech>
2025-06-06 17:21:18 +0200

    fix deploy, maybe

--- hosts/common/core/general.nix
+++ hosts/common/core/general.nix
@@ -56,7 +56,7 @@
       min-free = 128000000; # 128MB
       max-free = 1000000000; # 1GB
 
-      trusted-users = [ "@wheel" ];
+      trusted-users = [ "@wheel" "jakobe" "deploy"];
       # Deduplicate and optimize nix store
       auto-optimise-store = true;
       warn-dirty = false;
