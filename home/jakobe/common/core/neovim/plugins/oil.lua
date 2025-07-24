92880a3057696f387052f94d2f28668edcfec573
JakobEdvardsson <jakob@edvardsson.tech>
2025-07-24 20:33:48 +0200

    feat: oil show pwd

--- home/jakobe/common/core/neovim/plugins/oil.lua
+++ home/jakobe/common/core/neovim/plugins/oil.lua
@@ -1,6 +1,20 @@
+-- Declare a global function to retrieve the current directory
+function _G.get_oil_winbar()
+  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+  local dir = require("oil").get_current_dir(bufnr)
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  else
+    -- If there is no current directory (e.g. over ssh), just show the buffer name
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
+
 require("oil").setup({
   win_options = {
-    signcolumn = "yes:2",
+    winbar = "%!v:lua.get_oil_winbar()",
+        signcolumn = "yes:2",
+
   },
 })
 
