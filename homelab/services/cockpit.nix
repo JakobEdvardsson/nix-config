f6df4be1a52545650c4ba7fd71e2c3eac6fe539b
Jakob Edvardsson <jakob@edvardsson.tech>
2025-08-17 16:48:36 +0200

    feat: add cockpit

---
+++ homelab/services/cockpit.nix
@@ -0,0 +1,42 @@
+{ config, lib, ... }:
+let
+  service = "cockpit";
+  cfg = config.homelab.services.${service};
+  homelab = config.homelab;
+in
+{
+  options.homelab.services.${service} = {
+    enable = lib.mkEnableOption { description = "Enable ${service}"; };
+    url = lib.mkOption {
+      type = lib.types.str;
+      default = "cockpit.${homelab.baseDomain}";
+    };
+    homepage.name = lib.mkOption {
+      type = lib.types.str;
+      default = "Cockpit";
+    };
+    homepage.description = lib.mkOption {
+      type = lib.types.str;
+      default = "Web-based graphical interface for servers";
+    };
+    homepage.icon = lib.mkOption {
+      type = lib.types.str;
+      default = "cockpit.svg";
+    };
+    homepage.category = lib.mkOption {
+      type = lib.types.str;
+      default = "Media";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    services.cockpit = {
+      enable = true;
+    };
+    services.caddy.virtualHosts."${cfg.url}" = {
+      useACMEHost = homelab.baseDomain;
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:${toString config.services.cockpit.port}
+      '';
+    };
+  };
+}
