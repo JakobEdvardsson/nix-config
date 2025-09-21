ca5ff01af303f7b30808767c9ad4b0766711cbf6
Jakob Edvardsson <jakob@edvardsson.tech>
2025-09-21 15:31:26 +0200

    borgbase backup

--- hosts/nixos/think/backup.nix
+++ hosts/nixos/think/backup.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 let
-  restic-backup-id = "38badeb9-7644-4857-9758-67172f61b2af";
+  healthcheck-restic-tower = "38badeb9-7644-4857-9758-67172f61b2af";
+  healthcheck-restic-borgbase = "2c971425-4415-4bec-be2a-e029c4757186";
   # Get all enabled homelab services with dataDirs
   enabledServices = lib.attrsets.filterAttrs
     (name: svc: svc ? enable && svc.enable && svc ? dataDirs)
@@ -15,6 +16,15 @@ in {
       owner = "restic";
       group = "users";
     };
+    resticThinkBorgbaseRepo = {
+      owner = "restic";
+      group = "users";
+    };
+    resticThinkBorgbaseRepoURL = {
+      owner = "restic";
+      group = "users";
+    };
+
   };
 
   users.users.restic = {
@@ -44,6 +54,7 @@ in {
       remote-tower-backup = {
         initialize = true;
         passwordFile = "${config.sops.secrets.resticThinkTowerRepo.path}";
+        repository = "rest:http://@tower:8000/think";
         user = "restic";
         package = pkgs.writeShellScriptBin "restic" ''
           exec /run/wrappers/bin/restic "$@"
@@ -53,15 +64,38 @@ in {
           "${config.services.postgresqlBackup.location}"
           "${config.services.grafana.dataDir}"
         ];
-        repository = "rest:http://@tower:8000/think";
         timerConfig = {
           OnCalendar = "00:00";
           RandomizedDelaySec = "5h";
         };
         backupPrepareCommand =
-          "${pkgs.curl}/bin/curl -m 10 --retry 5 https://${config.homelab.services.healthchecks.url}/ping/${restic-backup-id}/start";
+          "${pkgs.curl}/bin/curl -m 10 --retry 5 https://${config.homelab.services.healthchecks.url}/ping/${healthcheck-restic-tower}/start";
         backupCleanupCommand =
-          "${pkgs.curl}/bin/curl -m 10 --retry 5 https://${config.homelab.services.healthchecks.url}/ping/${restic-backup-id}/$EXIT_STATUS";
+          "${pkgs.curl}/bin/curl -m 10 --retry 5 https://${config.homelab.services.healthchecks.url}/ping/${healthcheck-restic-tower}/$EXIT_STATUS";
+      };
+      remote-borgbase-backup = {
+        initialize = true;
+        passwordFile = "${config.sops.secrets.resticThinkBorgbaseRepo.path}";
+        repositoryFile =
+          "${config.sops.secrets.resticThinkBorgbaseRepoURL.path}";
+        user = "restic";
+        package = pkgs.writeShellScriptBin "restic" ''
+          exec /run/wrappers/bin/restic "$@"
+        '';
+        # paths = allDataDirs ++ [
+        paths = [
+          "${config.services.mysqlBackup.location}"
+          "${config.services.postgresqlBackup.location}"
+          "${config.services.grafana.dataDir}"
+        ];
+        timerConfig = {
+          OnCalendar = "00:00";
+          RandomizedDelaySec = "5h";
+        };
+        backupPrepareCommand =
+          "${pkgs.curl}/bin/curl -m 10 --retry 5 https://${config.homelab.services.healthchecks.url}/ping/${healthcheck-restic-borgbase}/start";
+        backupCleanupCommand =
+          "${pkgs.curl}/bin/curl -m 10 --retry 5 https://${config.homelab.services.healthchecks.url}/ping/${healthcheck-restic-borgbase}/$EXIT_STATUS";
       };
     };
   };
