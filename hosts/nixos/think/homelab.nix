169007a2c4a8f2f858307fe6a693d7203c4e1941
Jakob Edvardsson <jakob@edvardsson.tech>
2025-09-27 19:56:40 +0200

    arr

--- hosts/nixos/think/homelab.nix
+++ hosts/nixos/think/homelab.nix
@@ -43,7 +43,7 @@ in {
         # radarr.enable = true;
         # sonarr.enable = true;
         # jellyseerr.enable = true;
-        # deluge.enable = true;
+        deluge.enable = true;
 
         immich.enable = true;
 
@@ -57,13 +57,57 @@ in {
         # adguard.enable = true;
         # syncthing.enable = true;
 
-        # wireguard-netns = {
-        #   enable = true;
-        #   configFile = config.sops.secrets.wireguardCredentials.path;
-        #   privateIP = "10.2.0.2";
-        #   dnsIP = "10.2.0.1";
-        # };
+        wireguard-netns = {
+          enable = true;
+          configFile = config.sops.secrets.wireguardCredentials.path;
+          privateIP = "10.2.0.2";
+          dnsIP = "10.2.0.1";
+        };
       };
     };
+
+    # torrent
+    fileSystems."/mnt/data" = {
+      device = "tower:/mnt/user/data";
+      fsType = "nfs";
+      options = [
+        "x-systemd.automount"
+        "x-systemd.idle-timeout=60"
+        "_netdev"
+        "nofail"
+        "noresvport"
+        "nfsvers=4"
+        "rsize=1048576"
+        "wsize=1048576"
+        "hard"
+        "timeo=600"
+        "retrans=2"
+      ];
+      neededForBoot = false;
+    };
+
+    systemd.services.deluged = {
+      after = [ "mnt-data.automount" "network-online.target" ];
+      requires = [ "mnt-data.automount" ];
+      bindsTo = [ "mnt-data.mount" ];
+      wantedBy = [ "mnt-data.automount" ];
+      restartIfChanged = true;
+      serviceConfig = {
+        Restart = lib.mkForce "on-failure";
+        RestartSec = 10;
+      };
+    };
+    systemd.services.deluge-web = {
+      after = [ "mnt-data.automount" "network-online.target" ];
+      requires = [ "mnt-data.automount" ];
+      bindsTo = [ "mnt-data.mount" ];
+      wantedBy = [ "mnt-data.automount" ];
+      restartIfChanged = true;
+      serviceConfig = {
+        Restart = lib.mkForce "on-failure";
+        RestartSec = 10;
+      };
+    };
+
   };
 }
