1b1cd9cff5fc97dbe02b01f1bee0dc9650720cd7
Jakob Edvardsson <jakob@edvardsson.tech>
2025-10-11 20:56:56 +0200

    Auto mount NFS share (#27)

--- lib/default.nix
+++ lib/default.nix
@@ -1,4 +1,4 @@
-{ lib, ... }: {
+{ lib, pkgs, ... }: {
   # use path relative to the root of the project
   relativeToRoot = lib.path.append ../.;
   scanPaths = path:
@@ -8,4 +8,68 @@
         || ((path != "default.nix") # ignore default.nix
           && (lib.strings.hasSuffix ".nix" path) # include .nix files
         )) (builtins.readDir path)));
+
+  # ------------------------------------------------------------
+  # addNfsMountWithAutomount
+  # ------------------------------------------------------------
+  # Usage:
+  #   (addNfsMountWithAutomount "/mnt/data" "tower:/mnt/user/data")
+  # Arguments:
+  #     where: The mount point directory, e.g., "/mnt/data"
+  #     what: The NFS share, e.g., "tower:/mnt/user/data"
+  # ------------------------------------------------------------
+  # inspo: https://github.com/systemd/systemd/issues/16811
+  addNfsMountWithAutomount = where: what:
+    let
+      unitName =
+        lib.replaceStrings [ "/" ] [ "-" ] (lib.removePrefix "/" where);
+    in {
+      boot.supportedFilesystems = [ "nfs" ];
+      services.rpcbind.enable = true;
+
+      fileSystems.${where} = {
+        device = what;
+        options = [
+          "defaults"
+          "x-systemd.automount"
+          "noauto"
+          "x-systemd.idle-timeout=60"
+          "x-systemd.device-timeout=5s"
+          "x-systemd.mount-timeout=5s"
+          "nofail"
+          "_netdev"
+        ];
+        fsType = "nfs";
+        neededForBoot = false;
+      };
+
+      systemd.mounts = [{
+        where = what;
+        unitConfig.OnFailure = "automount-restarter@${unitName}.service";
+      }];
+
+      systemd.services."automount-restarter@" = {
+        description = "automount restarter for %i";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStartPre = "${pkgs.coreutils}/bin/sleep 10";
+          ExecStart = "${pkgs.systemd}/bin/systemctl restart %i.automount";
+        };
+      };
+
+      #  #### Make service depend on the mount
+      #  systemd.services.${serviceName} = {
+      #    after = [ "${unitName}.automount" "network-online.target" ];
+      #    wants = [ "network-online.target" ];
+      #    requires = [ "${unitName}.automount" ];
+      #    bindsTo = [ "${unitName}.mount" ];
+      #    wantedBy = [ "multi-user.target" ];
+      #    restartIfChanged = true;
+      #
+      #    serviceConfig = {
+      #      Restart = lib.mkForce "on-failure";
+      #      RestartSec = 60;
+      #    };
+      #  };
+    };
 }
