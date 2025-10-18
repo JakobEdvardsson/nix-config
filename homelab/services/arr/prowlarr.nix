42aa47e8d98f55f239e28b8f5d58e15174056d6a
JakobEdvardsson <jakob@edvardsson.tech>
2025-10-18 17:22:42 +0200

    add prowlarr

--- homelab/services/arr/prowlarr.nix
+++ homelab/services/arr/prowlarr.nix
@@ -1,43 +1,29 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   service = "prowlarr";
-  cfg = config.homelab.services.${service};
   homelab = config.homelab;
+  cfg = config.homelab.services.${service};
+  optionsFn = import ../../options.nix;
 in {
-  options.homelab.services.${service} = {
-    enable = lib.mkEnableOption { description = "Enable ${service}"; };
-    configDir = lib.mkOption {
-      type = lib.types.str;
-      default = "/var/lib/${service}";
-    };
-    url = lib.mkOption {
-      type = lib.types.str;
-      default = "${service}.${homelab.baseDomain}";
-    };
-    homepage.name = lib.mkOption {
-      type = lib.types.str;
-      default = "Prowlarr";
-    };
-    homepage.description = lib.mkOption {
-      type = lib.types.str;
-      default = "PVR indexer";
-    };
-    homepage.icon = lib.mkOption {
-      type = lib.types.str;
-      default = "prowlarr.svg";
-    };
-    homepage.category = lib.mkOption {
-      type = lib.types.str;
-      default = "Arr";
+  options.homelab.services.${service} = optionsFn {
+    inherit lib service config homelab;
+    homepage = {
+      description = "Torrent indexer";
+      category = "Arr";
     };
   };
-  config = lib.mkIf cfg.enable {
-    services.${service} = { enable = true; };
-    services.caddy.virtualHosts."${cfg.url}" = {
-      useACMEHost = homelab.baseDomain;
-      extraConfig = ''
-        reverse_proxy http://127.0.0.1:9696
-      '';
-    };
-  };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      sops.secrets = { "${service}ApiKey" = { }; };
+
+      services.${service} = { enable = true; };
+
+      services.caddy.virtualHosts."${cfg.url}" = {
+        useACMEHost = homelab.baseDomain;
+        extraConfig = ''
+          reverse_proxy http://127.0.0.1:9696
+        '';
+      };
+    })
+  ];
 }
