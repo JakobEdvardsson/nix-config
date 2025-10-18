6e83215a0c40d33054f53dd53f5549495261ac77
JakobEdvardsson <jakob@edvardsson.tech>
2025-10-18 17:35:43 +0200

    add flaresolverr

---
+++ homelab/services/arr/flaresolverr.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }:
+let
+  service = "flaresolverr";
+  homelab = config.homelab;
+  cfg = config.homelab.services.${service};
+  optionsFn = import ../../options.nix;
+in {
+  options.homelab.services.${service} = optionsFn {
+    inherit lib service config homelab;
+    homepage = {
+      description = "Proxy server to bypass Cloudflare protection";
+      category = "Arr";
+    };
+  };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      sops.secrets = { "${service}ApiKey" = { }; };
+
+      services.${service} = { enable = true; };
+
+      services.caddy.virtualHosts."${cfg.url}" = {
+        useACMEHost = homelab.baseDomain;
+        extraConfig = ''
+          reverse_proxy http://127.0.0.1:8191
+        '';
+      };
+    })
+  ];
+}
