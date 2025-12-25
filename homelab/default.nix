eb666ec21ea9dad0d155d37e323b2c3cb195f51f
JakobEdvardsson <jakob@edvardsson.tech>
2025-12-25 21:11:37 +0100

    move caddy stuff

--- homelab/default.nix
+++ homelab/default.nix
@@ -64,16 +64,64 @@ in
     # ./networks
     # ./motd
   ];
-  config = lib.mkIf cfg.enable {
-    users = {
-      groups.${cfg.group} = {
-        gid = 993;
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      users = {
+        groups.${cfg.group} = {
+          gid = 993;
+        };
+        users.${cfg.user} = {
+          uid = 994;
+          isSystemUser = true;
+          group = cfg.group;
+        };
       };
-      users.${cfg.user} = {
-        uid = 994;
-        isSystemUser = true;
-        group = cfg.group;
+    })
+    (lib.mkIf config.homelab.services.enable {
+      assertions = [
+        {
+          assertion = cfg.enable;
+          message = "homelab.services.enable requires homelab.enable to be set to true.";
+        }
+      ];
+
+      networking.firewall.allowedTCPPorts = [
+        80
+        443
+      ];
+      security.acme = {
+        acceptTerms = true;
+        defaults.email = config.hostSpec.email;
+        certs.${config.homelab.baseDomain} = {
+          reloadServices = [ "caddy.service" ];
+          domain = "${config.homelab.baseDomain}";
+          extraDomainNames = [ "*.${config.homelab.baseDomain}" ];
+          dnsProvider = "cloudflare";
+          dnsResolver = "1.1.1.1:53";
+          dnsPropagationCheck = true;
+          group = config.services.caddy.group;
+          environmentFile = config.homelab.cloudflare.dnsCredentialsFile;
+        };
       };
-    };
-  };
+
+      services.caddy = {
+        enable = true;
+        globalConfig = ''
+          auto_https off
+        '';
+        # virtualHosts = {
+        #   "http://${config.homelab.baseDomain}" = {
+        #     extraConfig = ''
+        #       redir https://{host}{uri}
+        #     '';
+        #   };
+        #   "http://*.${config.homelab.baseDomain}" = {
+        #     extraConfig = ''
+        #       redir https://{host}{uri}
+        #     '';
+        #   };
+        #};
+      };
+    })
+  ];
 }
