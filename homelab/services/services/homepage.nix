a693d959da8b446828a4082e64c9c74f91e6f39b
JakobEdvardsson <jakob@edvardsson.tech>
2025-12-27 23:17:53 +0100

    homepage default css

--- homelab/services/services/homepage.nix
+++ homelab/services/services/homepage.nix
@@ -53,8 +53,7 @@ in
     services.${nixosService} =
       let
         homepageEnabledServices = lib.attrsets.filterAttrs (
-          name: value:
-          value ? enable && value.enable && value ? homepage && value.homepage.show
+          name: value: value ? enable && value.enable && value ? homepage && value.homepage.show
         ) homelab.services;
         homelabCategories = lib.lists.unique (
           lib.attrsets.mapAttrsToList (_: value: value.homepage.category) homepageEnabledServices
@@ -85,30 +84,6 @@ in
       {
         enable = true;
         allowedHosts = cfg.allowedHosts;
-        customCSS = ''
-          body, html {
-            font-family: SF Pro Display, Helvetica, Arial, sans-serif !important;
-          }
-          .font-medium {
-            font-weight: 700 !important;
-          }
-          .font-light {
-            font-weight: 500 !important;
-          }
-          .font-thin {
-            font-weight: 400 !important;
-          }
-          #information-widgets {
-            padding-left: 1.5rem;
-            padding-right: 1.5rem;
-          }
-          div#footer {
-            display: none;
-          }
-          .services-group.basis-full.flex-1.px-1.-my-1 {
-            padding-bottom: 3rem;
-          };
-        '';
         settings = {
           layout = [
             {
